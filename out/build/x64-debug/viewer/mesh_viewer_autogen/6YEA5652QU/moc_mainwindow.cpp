283a6ad10c89319e0870bf1c341918a134ff680f
InsistentWind <578009720@qq.com>
2025-10-06 12:07:19 +0800

    update_25_10_6

--- out/build/x64-debug/viewer/mesh_viewer_autogen/6YEA5652QU/moc_mainwindow.cpp
+++ out/build/x64-debug/viewer/mesh_viewer_autogen/6YEA5652QU/moc_mainwindow.cpp
@@ -45,7 +45,9 @@ template <> constexpr inline auto MainWindow::qt_create_metaobjectdata<qt_meta_t
         "vertices",
         "std::vector<uint>",
         "indices",
-        "openFile"
+        "openFile",
+        "restoreModel",
+        "requestProcess"
     };
 
     QtMocHelpers::UintData qt_methods {
@@ -55,6 +57,10 @@ template <> constexpr inline auto MainWindow::qt_create_metaobjectdata<qt_meta_t
         }}),
         // Slot 'openFile'
         QtMocHelpers::SlotData<void()>(7, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'restoreModel'
+        QtMocHelpers::SlotData<void()>(8, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'requestProcess'
+        QtMocHelpers::SlotData<void()>(9, 2, QMC::AccessPrivate, QMetaType::Void),
     };
     QtMocHelpers::UintData qt_properties {
     };
@@ -80,6 +86,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->objLoaded((*reinterpret_cast< std::add_pointer_t<std::vector<QVector3D>>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<std::vector<uint>>>(_a[2]))); break;
         case 1: _t->openFile(); break;
+        case 2: _t->restoreModel(); break;
+        case 3: _t->requestProcess(); break;
         default: ;
         }
     }
@@ -108,14 +116,14 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 4;
     }
     if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 4)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 2;
+        _id -= 4;
     }
     return _id;
 }
